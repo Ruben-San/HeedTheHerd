b03dfd48eb408aff3ea258e7fb74f11e7ef00dfa
Ruben Sanchez <rfv3@yahoo.com>
2015-04-08 23:00:12 -0600

    updated root path

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
         sessions: 'sessions'
       }
-  root 'layout#index'
+  root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
