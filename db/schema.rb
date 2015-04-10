797344900c20539cb481e90f316643173f817c72
Ariana <dorado_ariana@yahoo.com>
2015-04-10 15:55:07 -0600

    Fixed users profile page and added better_errors and binding of caller to gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408204806) do
+ActiveRecord::Schema.define(version: 20150410214810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,10 @@ ActiveRecord::Schema.define(version: 20150408204806) do
     t.string   "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "horses", ["user_id"], name: "index_horses_on_user_id", using: :btree
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
@@ -57,4 +60,5 @@ ActiveRecord::Schema.define(version: 20150408204806) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "horses", "users"
 end
