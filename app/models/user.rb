a2d520e7b2daef618270cf2dc3d586390f196f7c
Ruben Sanchez <rfv3@yahoo.com>
2015-04-13 11:58:09 -0600

    fixed error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :horses, dependent: :destroy
-  has_many :tasks, through: => :horses
+  has_many :tasks, through: :horses
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
