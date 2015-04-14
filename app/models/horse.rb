ba8fa1753b7468ff52eef514bf402397f166ea49
Ruben Sanchez <rfv3@yahoo.com>
2015-04-14 14:00:11 -0600

    updated validations for paperclip

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -3,4 +3,5 @@ class Horse < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
   has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :pic, :content_type => /\Aimage\/.*\Z/
+  validates_with AttachmentSizeValidator, :attributes => :pic, :less_than => 1.megabytes
 end
