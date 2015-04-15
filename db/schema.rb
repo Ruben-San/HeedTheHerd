195188a86f0f6f72befe1a08b5130c4370f169ee
Nicole Radford <nradford0171@gmail.com>
2015-04-15 14:17:11 -0600

    droped and recreated db

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150413215855) do
-
+ActiveRecord::Schema.define(version: 20150413202628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,13 +34,6 @@ ActiveRecord::Schema.define(version: 20150413215855) do
   end
 
   add_index "horses", ["user_id"], name: "index_horses_on_user_id", using: :btree
-
-  create_table "task_assignments", force: :cascade do |t|
-    t.integer  "horse_id"
-    t.integer  "task_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
