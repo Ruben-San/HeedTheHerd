af6c54cf743e9f255f794380cfb96d18fb77e539
Ruben Sanchez <rfv3@yahoo.com>
2015-04-15 22:53:22 -0600

    added a default pony picture/ fixed some bugs in code

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -3,7 +3,7 @@ class Horse < ActiveRecord::Base
 
   has_many :tasks, dependent: :destroy
   validates :name, :breed, :RHR,  presence: true, length: { maximum: 50 }
-  has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => ":style/missing.png"
   validates_attachment_content_type :pic, :content_type => /\Aimage\/.*\Z/
 
 end
