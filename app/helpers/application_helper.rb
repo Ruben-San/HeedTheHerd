0360492d87d22e923b0ad8423f09073aff03ea1d
Nicole Radford <nradford0171@gmail.com>
2015-04-18 14:47:16 -0600

    no changes made to this file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,2 @@
-require HTTParty
-
 module ApplicationHelper
-  
-
-  #def parameters
-    #User.each do |user|
-     # q = user.zipcode
-      #num_of_days = 1
-      #tp = 24
-    
-  end
-
-  response= get(api.worldweatheronline.com/free/v2/weather.ashx?q=23192&format=json&num_of_days=1&date=today&tp=tp%3D24&key=31f6ddb631ced9c600aad9ab924d7)
-  puts response.body
 end
