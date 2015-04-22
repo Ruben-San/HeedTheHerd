ecab2b7d563360383e4dc1ca053c7caa751cb84e
Ariana <dorado_ariana@yahoo.com>
2015-04-22 14:37:40 -0600

    Password only has to be confirmed now.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,4 +24,10 @@ class UsersController < ApplicationController
   def show
 
   end
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :zipcode)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
   has_many :horses, dependent: :destroy
   has_many :tasks, through: :horses
-  validates :email, :password, presence: true
+  before_save   :downcase_email  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  
   after_create :send_welcome_email
 
   # Include default devise modules. Others available are:
@@ -9,13 +14,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
+  default_scope { order(:email) }
 
   private
+    def downcase_email
+      self.email = email.downcase
+    end
+
     def send_welcome_email
       UserMailer.welcome_email(self).deliver_later
     end
-
-
 
 end
