aa980a1dd3857a362a658f9d3800510d4220c283
Ariana <dorado_ariana@yahoo.com>
2015-04-30 17:07:41 -0600

    Changed background and line color on chart

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -2,8 +2,8 @@ Chartkick.options = {
       width: '100%',
       height: "300px",
   library: {
-    backgroundColor: "#eee",
-    colors: ["#ff0000", "#00ff00", "#0000ff", "#ffff00", "#ff00ff", "0000ff"],
+    backgroundColor: "#EAE7E5",
+    colors: ["#777A5F"],
       chartArea: {
       left: 100,
       top: 10,
