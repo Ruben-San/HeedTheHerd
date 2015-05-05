e29ea136360b430fee06d61b0b6a535a8d44b2a8
Ariana <dorado_ariana@yahoo.com>
2015-05-05 01:07:28 -0600

    Worked on getting images to show up in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,4 +104,7 @@ config.action_mailer.raise_delivery_errors = true
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+config.serve_static_assets = false
 end
